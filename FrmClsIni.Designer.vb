274a8ce769058a5dbc720f8f70c8f0b1718307fe
x-310 <x310@outlook.jp>
2023-01-10 23:19:10 +0900

    プロジェクト ファイルを追加します。

---
+++ FrmClsIni.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmClsIni
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(566, 143)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(280, 132)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "iniファイルを読み込む"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(96, 73)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(384, 275)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'FrmClsIni
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(961, 457)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.Name = "FrmClsIni"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FrmClsIni"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As Windows.Forms.RichTextBox
+End Class
---
+++ FrmClsIni.vb
@@ -0,0 +1,58 @@
+﻿' System.Runtime.InteropServicesクラスをインポート
+Imports System.Runtime.InteropServices
+
+Public Class FrmClsIni
+    ' iniファイルの場所を定数として宣言
+    Private Const INI_FILE_PATH As String = "./ini/test.ini"
+
+    ' API宣言
+    ' ------------------------------------------------------
+    ' ini ファイル書き込み
+    ' ------------------------------------------------------
+    <DllImport("Kernel32.dll")>
+    Private Shared Function WritePrivateProfileString(
+                    ByVal lpAppName As String,                      'セクション名
+                    ByVal lpKeyName As String,                      'キー名
+                    ByVal lpString As String,
+                    ByVal lpFileName As String) As Integer
+    End Function
+
+    ' ------------------------------------------------------
+    ' ini ファイル読み込み
+    ' ------------------------------------------------------
+    <DllImport("Kernel32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function GetPrivateProfileString(
+                    ByVal lpAppName As String,                                              ' セクション名
+                    ByVal lpKeyName As String,                                              ' キー名
+                    ByVal lpDefault As String,                                              ' キーが見つからなかった場合に取得するデフォルト値
+                    ByVal lpReturnedString As System.Text.StringBuilder,                    ' 取得した文字列が入るバッファ
+                    ByVal nSize As Integer,                                                 ' 取得した文字列のバッファサイズ
+                    ByVal lpFileName As String) As Integer
+    End Function
+
+    ''' <summary>
+    ''' INIファイル読取(文字列)
+    ''' </summary>
+    ''' <param name="lpAppName"></param>
+    ''' <param name="lpKeyName"></param>
+    ''' <returns></returns>
+    Public Function GetIniString(ByVal lpAppName As String, ByVal lpKeyName As String) As String
+        Dim BuffSize As Integer = 256
+        Dim str As System.Text.StringBuilder = New System.Text.StringBuilder(BuffSize)
+        Try
+            Call GetPrivateProfileString(lpAppName, lpKeyName, str.ToString, str, str.Capacity, INI_FILE_PATH)
+            Return str.ToString
+        Catch ex As Exception
+            Return str.ToString
+        End Try
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ' iniファイルを読み込む
+        Me.RichTextBox1.Text = GetIniString("TEST", "key")
+        Me.RichTextBox1.Text += vbCrLf
+        Me.RichTextBox1.Text += GetIniString("TEST", "key2")
+
+    End Sub
+
+End Class
